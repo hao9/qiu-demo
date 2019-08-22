b4a66094783e687b362fb6bd36bc9efba53f18b3
haoshengsheng <576531447@qq.com>
2019-08-22 12:23:14 +0800

    基础环境部署

---
+++ qiu-demo-user/src/main/java/com/hao/demo/qiu/QiuApplication.java
@@ -0,0 +1,13 @@
+package com.hao.demo.qiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiuApplication.class, args);
+    }
+
+}
---
+++ qiu-demo-user/src/main/java/com/hao/demo/qiu/service/UserService.java
@@ -0,0 +1,9 @@
+package com.hao.demo.qiu.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
